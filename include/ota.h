090e7e92648bf0a17c099834abcc45e3b0caaf1e
arthoog <github@elfkes.nl>
2021-01-16 00:23:38 +0100

    ota separate files

---
+++ include/ota.h
@@ -0,0 +1,2 @@
+void ota_init(void);
+void ota_loop(void);
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 #include <WiFiUdp.h>
-#include <ArduinoOTA.h>
+// #include <ArduinoOTA.h>
+#include "ota.h"
 #include "AP-elfjes.h"
 #include "mqtt.h"
 #include <Streaming.h>
@@ -194,26 +195,7 @@ void setup()
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());
 
-    //OTA spul
-    // ArduinoOTA.setPort(8266);                        // Port defaults to 8266
-    ArduinoOTA.setHostname("SDM120M");                  // Hostname defaults to esp8266-[ChipID]
-    //ArduinoOTA.setPassword((const char *)"geheim");   // No authentication by default
-    ArduinoOTA.onStart([]() { Serial.println("Start"); });
-    ArduinoOTA.onEnd([]()   { Serial.println("\nEnd"); });
-    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) 
-    { 
-        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    });
-    ArduinoOTA.onError([](ota_error_t error) 
-    {
-        Serial.printf("Error[%u]: ", error);
-        if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-        else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-        else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-        else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-        else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
-    ArduinoOTA.begin();
+    ota_init();
 
     //MQTT spul
     Serial << "#mqttserver=" << mqtt_server << endl;
@@ -243,7 +225,8 @@ void setup()
 //---------------------------------------------------------------
 void loop() 
 {
-    ArduinoOTA.handle();
+    // ArduinoOTA.handle();
+    ota_loop();
 
     mqtt_loop();
 
